3e532c7f9ebd6c2bc87772b0b2f803c20c4f52bc
venu kommineni <venukommineni615@gmail.com>
2023-09-07 14:36:13 +0530

    dist added

--- Course-app/src/components/UpdateCourses.tsx
+++ Course-app/src/components/UpdateCourses.tsx
@@ -103,6 +103,7 @@ function EditCourse({title,setTitle,description,setDescription,image,setImage,pr
                     {headers:{
                         "Authorization":localStorage.getItem("token")
                     }});
+                        alert(update)
                         navigate("/courses")
                     }}
     
